690910e041ed0efc15747cc6419446b8c197ab83
Marco Tabini <marcot@marcotabinisimac-3.local>
2014-10-21 13:19:12 -0400

    Added new Helper

---
+++ agent/job/helper.go
@@ -0,0 +1,97 @@
+package job
+
+import (
+	"github.com/telemetryapp/gotelemetry"
+	"sync"
+	"time"
+)
+
+type PluginHelperClosure func(job *Job)
+type PluginHelperClosureWithFlow func(job *Job, f *gotelemetry.Flow)
+
+type PluginHelperTask func(job *Job, doneChannel chan bool)
+
+type PluginHelper struct {
+	Tasks       []PluginHelperTask
+	DoneChannel chan bool
+	WaitGroup   *sync.WaitGroup
+}
+
+func NewPluginHelper() *PluginHelper {
+	return &PluginHelper{
+		Tasks:       []PluginHelperTask{},
+		DoneChannel: make(chan bool, 0),
+		WaitGroup:   &sync.WaitGroup{},
+	}
+}
+
+func (e *PluginHelper) AddTask(t PluginHelperTask) {
+	e.Tasks = append(e.Tasks, t)
+}
+
+func (e *PluginHelper) AddTaskWithClosure(c PluginHelperClosure, interval time.Duration) {
+	t := func(job *Job, doneChannel chan bool) {
+		t := time.NewTimer(interval)
+		t.Stop()
+
+		for {
+			c(job)
+
+			t.Reset(interval)
+
+			select {
+			case <-doneChannel:
+				t.Stop()
+				return
+
+			case <-t.C:
+				break
+			}
+		}
+	}
+
+	e.AddTask(t)
+}
+
+func (e *PluginHelper) AddTaskWithClosureForFlowWithName(c PluginHelperClosureWithFlow, interval time.Duration, flows map[string]*gotelemetry.Flow, name, prefix string) error {
+	f, found := flows[prefix+name]
+
+	if !found {
+		return gotelemetry.NewError(400, "Flow "+prefix+name+" not found.")
+	}
+
+	closure := func(job *Job) {
+		c(job, f)
+	}
+
+	e.AddTaskWithClosure(closure, interval)
+
+	return nil
+}
+
+func (e *PluginHelper) Run(job *Job) {
+	defer e.WaitGroup.Done()
+
+	for _, t := range e.Tasks {
+		e.WaitGroup.Add(1)
+
+		go func(t PluginHelperTask) {
+			t(job, e.DoneChannel)
+			e.WaitGroup.Done()
+		}(t)
+	}
+
+	select {
+	case <-e.DoneChannel:
+		return
+	}
+}
+
+func (e *PluginHelper) Reconfigure(job *Job, config map[string]interface{}) error {
+	return gotelemetry.NewError(400, "This pluginc cannot reconfigure itself.")
+}
+
+func (e *PluginHelper) Terminate(job *Job) {
+	e.DoneChannel <- true
+	e.WaitGroup.Wait()
+}
--- plugin/random.go
+++ plugin/random.go
@@ -21,15 +21,15 @@ type RandomPluginFactory struct {
 
 // NewInstance generates a blank plugin instance
 func (r *RandomPluginFactory) NewInstance() job.PluginInstance {
-	return &RandomPlugin{}
+	return &RandomPlugin{
+		job.NewPluginHelper(),
+	}
 }
 
 // Struct RandomPlugin is our sample plugin. It takes a config that provides the name and prefix of a board and
 // attempts to create that board inside the account; it then feeds random data to the board on a schedule.
 type RandomPlugin struct {
-	prefix string                       // The board's prefix
-	board  *gotelemetry.Board           // The board
-	flows  map[string]*gotelemetry.Flow // A map of flows—handy for quick reference
+	*job.PluginHelper
 }
 
 // Init initializes the plugin. Perform whatever initialization you need here.
@@ -44,8 +44,6 @@ func (r *RandomPlugin) Init(job *job.Job, config map[string]interface{}) error {
 	boardName := board["name"].(string)
 	boardPrefix := board["prefix"].(string)
 
-	r.prefix = boardPrefix
-
 	// This template was generated using gotelemetry/dumpboard
 	template := "{\"name\":\"RandomTest\",\"theme\":\"dark\",\"aspect_ratio\":\"HDTV\",\"font_family\":\"normal\",\"font_size\":\"normal\",\"widget_background\":\"\",\"widget_margins\":3,\"widget_padding\":8,\"widgets\":[{\"variant\":\"value\",\"tag\":\"value_98\",\"column\":7,\"row\":7,\"width\":8,\"height\":5,\"in_board_index\":0,\"background\":\"default\"},{\"variant\":\"value\",\"tag\":\"value_99\",\"column\":17,\"row\":7,\"width\":8,\"height\":5,\"in_board_index\":1,\"background\":\"default\"}]}"
 
@@ -58,74 +56,38 @@ func (r *RandomPlugin) Init(job *job.Job, config map[string]interface{}) error {
 
 	// Map the board's widgets to their respective flows and pre-populate
 	// the flows with their current values
-	r.flows, err = b.MapWidgetsToFlows()
+	flows, err := b.MapWidgetsToFlows()
 
 	if err != nil {
 		return err
 	}
 
-	// Save the board into the plugin instance
-	r.board = b
+	if err = r.PluginHelper.AddTaskWithClosureForFlowWithName(FeedValueFlow, time.Second*5, flows, "value_98", boardPrefix); err != nil {
+		return err
+	}
+
+	if err = r.PluginHelper.AddTaskWithClosureForFlowWithName(FeedValueFlow, time.Second*7, flows, "value_99", boardPrefix); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-// Run executes the plugin instance. Within the context of the plugin, this method should be considered
-// synchronous, and should _never_ return unless the plugin is terminated. It is up to the plugin
-// to determine what needs to happen in here.
-func (r *RandomPlugin) Run(job *job.Job) {
-	// Run forever
-	for {
-		// Grab a flow
-		tag := r.prefix + "value_98"
-		flow := r.flows[tag]
-		data, err := flow.ValueData()
+func FeedValueFlow(job *job.Job, f *gotelemetry.Flow) {
+	data, err := f.ValueData()
 
-		if !err {
-			// Report an error this way to ensure proper logging
-			job.ReportError(gotelemetry.NewError(500, "Cannot find flow with tag `"+tag+"`"))
-			return
-		}
-
-		data.Value = rand.Float64() * 10000
-
-		// Post a flow update this way. Updates are automatically accumulated and submitted to
-		// Telemetry according to the schedule illustrated in the config
-		job.PostFlowUpdate(flow)
-
-		// Log to the global log this way.
-		job.Logf("Updated flow %s", flow.Tag)
-
-		tag = r.prefix + "value_99"
-		flow = r.flows[tag]
-		data, err = flow.ValueData()
-
-		if !err {
-			job.ReportError(gotelemetry.NewError(500, "Cannot find flow with tag `"+tag+"`"))
-			return
-		}
-
-		data.Value = rand.Float64() * 10000
-
-		job.PostFlowUpdate(flow)
-
-		job.Logf("Updated flow %s", flow.Tag)
-
-		// Sleep 5 seconds
-		time.Sleep(time.Second * 5)
+	if !err {
+		// Report an error this way to ensure proper logging
+		job.ReportError(gotelemetry.NewError(500, "Cannot extract value data from flow with tag `"+f.Tag+"`"))
+		return
 	}
-}
 
-// Reconfigure reconfigures the plugin if possible. If the plugin does not support
-// reconfiguration, it can return a gotelemetry.Error with statusCode set to 400,
-// in which case the current plugin instance will be torn down and a new one will
-// be created.
-func (r *RandomPlugin) Reconfigure(job *job.Job, config map[string]interface{}) error {
-	return gotelemetry.NewError(400, "Cannot reconfigure")
-}
+	data.Value = rand.Float64() * 10000
 
-// Terminates terminates the plugin. Note that this method should be considered
-// synchronous; when it exits, the plugin will be destroyed.
-func (r *RandomPlugin) Terminate(job *job.Job) {
+	// Post a flow update this way. Updates are automatically accumulated and submitted to
+	// Telemetry according to the schedule illustrated in the config
+	job.PostFlowUpdate(f)
 
+	// Log to the global log this way.
+	job.Logf("Updated flow %s", f.Tag)
 }
