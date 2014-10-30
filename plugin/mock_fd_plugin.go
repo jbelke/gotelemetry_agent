743fe0e5f3988ed859f380a36eef3d0b03ff3a24
Matheus Felipe <mfelipe@e-xact.com>
2014-10-30 16:31:04 -0700

    Adds timeseries placeholders

--- plugin/mock_fd_plugin.go
+++ plugin/mock_fd_plugin.go
@@ -22,7 +22,7 @@ func init() {
 	job.RegisterPlugin("plugin_fd", MockFdPluginFactory)
 
 	if dbErr != nil {
-		job.ReportError(gotelemetry.NewError(500, "Plugin 'plugin_fd' could not connect to the database"))
+		log.Fatal(gotelemetry.NewError(500, "Plugin 'plugin_fd' could not connect to the database"))
 	}
 }
 
@@ -39,8 +39,8 @@ func (plugin *MockFdPlugin) Init(job *job.Job) error {
 	boardName := configBoard["name"].(string)
 	boardPrefix := configBoard["prefix"].(string)
 
-	template := "{\"name\":\"StandardBoardTest\",\"theme\":\"dark\",\"aspect_ratio\":\"HDTV\",\"font_family\":\"normal\",\"font_size\":\"normal\",\"widget_background\":\"\",\"widget_margins\":3,\"widget_padding\":8,\"widgets\":[{\"flow\":{\"tag\":\"upstatus_5\",\"data\":{\"up\":[\"Server1\",\"Server2\"]}},\"variant\":\"upstatus\",\"column\":0,\"row\":13,\"width\":5,\"height\":7,\"in_board_index\":0,\"background\":\"default\"},{\"flow\":{\"tag\":\"graph_15\",\"data\":{\"renderer\":\"line\",\"series\":[{\"values\":[15,20,88,12,45,33,55,75,50,53]}]}},\"variant\":\"graph\",\"column\":23,\"row\":13,\"width\":9,\"height\":7,\"in_board_index\":1,\"background\":\"default\"},{\"flow\":{\"tag\":\"graph_13\",\"data\":{\"renderer\":\"line\",\"series\":[{\"values\":[79,44,9,60,21,69,28,24,73,30]}]}},\"variant\":\"graph\",\"column\":5,\"row\":13,\"width\":9,\"height\":7,\"in_board_index\":2,\"background\":\"default\"},{\"flow\":{\"tag\":\"graph_14\",\"data\":{\"renderer\":\"line\",\"series\":[{\"values\":[15,20,88,12,45,33,55,75,50,53]}]}},\"variant\":\"graph\",\"column\":14,\"row\":13,\"width\":9,\"height\":7,\"in_board_index\":3,\"background\":\"default\"},{\"flow\":{\"tag\":\"text_3\",\"data\":{\"alignment\":\"center\",\"text\":\"Message 1\"}},\"variant\":\"text\",\"column\":0,\"row\":7,\"width\":32,\"height\":3,\"in_board_index\":4,\"background\":\"default\"},{\"flow\":{\"tag\":\"image_13\",\"data\":{\"mode\":\"fit\",\"url\":\"https://www.telemetryapp.com/img/telemetry-logo-header.png\"}},\"variant\":\"image\",\"column\":0,\"row\":0,\"width\":6,\"height\":7,\"in_board_index\":5,\"background\":\"default\"},{\"flow\":{\"tag\":\"gauge_22\",\"data\":{\"max\":100,\"value\":50}},\"variant\":\"gauge\",\"column\":26,\"row\":0,\"width\":6,\"height\":7,\"in_board_index\":6,\"background\":\"default\"},{\"flow\":{\"tag\":\"timeseries_10\",\"data\":{\"interval\":\"hours\",\"interval_count\":0,\"renderer\":\"line\",\"series_metadata\":[{\"aggregation\":\"avg\"}],\"values\":[100,200,300]}},\"variant\":\"timeseries\",\"column\":16,\"row\":0,\"width\":10,\"height\":7,\"in_board_index\":7,\"background\":\"default\"},{\"flow\":{\"tag\":\"timeseries_9\",\"data\":{\"interval\":\"hours\",\"interval_count\":0,\"renderer\":\"line\",\"series_metadata\":[{\"aggregation\":\"avg\"}],\"values\":[100,200,300]}},\"variant\":\"timeseries\",\"column\":6,\"row\":0,\"width\":10,\"height\":7,\"in_board_index\":8,\"background\":\"default\"},{\"flow\":{\"tag\":\"value_50\",\"data\":{\"value\":100}},\"variant\":\"value\",\"column\":0,\"row\":9,\"width\":11,\"height\":4,\"in_board_index\":9,\"background\":\"default\"},{\"flow\":{\"tag\":\"value_51\",\"data\":{\"value\":100}},\"variant\":\"value\",\"column\":11,\"row\":9,\"width\":10,\"height\":4,\"in_board_index\":10,\"background\":\"default\"},{\"flow\":{\"tag\":\"value_52\",\"data\":{\"value\":100}},\"variant\":\"value\",\"column\":21,\"row\":9,\"width\":11,\"height\":4,\"in_board_index\":11,\"background\":\"default\"}]}"
-
+	template :=	"{\"name\":\"StandarBoardTest\",\"theme\":\"dark\",\"aspect_ratio\":\"HDTV\",\"font_family\":\"normal\",\"font_size\":\"normal\",\"widget_background\":\"\",\"widget_margins\":3,\"widget_padding\":8,\"widgets\":[{\"flow\":{\"tag\":\"upstatus_5\",\"data\":{\"up\":[\"Server1\",\"Server2\"]}},\"variant\":\"upstatus\",\"column\":0,\"row\":13,\"width\":5,\"height\":7,\"in_board_index\":0,\"background\":\"default\"},{\"flow\":{\"tag\":\"graph_15\",\"data\":{\"renderer\":\"line\",\"series\":[{\"values\":[15,20,88,12,45,33,55,75,50,53]}]}},\"variant\":\"graph\",\"column\":23,\"row\":13,\"width\":9,\"height\":7,\"in_board_index\":1,\"background\":\"default\"},{\"flow\":{\"tag\":\"graph_13\",\"data\":{\"renderer\":\"line\",\"series\":[{\"values\":[9,18,18,18,18,18,19,18,18,20,18,17,17,18,18,19,18,19,20,18,18,18,18,18,18,18,18,18,18,18]}],\"title\":\"30 Day Transaction Volume\"}},\"variant\":\"graph\",\"column\":5,\"row\":13,\"width\":9,\"height\":7,\"in_board_index\":2,\"background\":\"default\"},{\"flow\":{\"tag\":\"graph_14\",\"data\":{\"renderer\":\"line\",\"series\":[{\"values\":[8,7,9,18,18,18,18,18,19,18,18,20,18,17,17,18,18,19,18,19,20,18,18,18,18,18,18,18,18,18,18,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}],\"title\":\"90 Day Transaction Volume\"}},\"variant\":\"graph\",\"column\":14,\"row\":13,\"width\":9,\"height\":7,\"in_board_index\":3,\"background\":\"default\"},{\"flow\":{\"tag\":\"text_3\",\"data\":{\"alignment\":\"center\",\"text\":\"Daily transaction volume - Today is: Oct 10, 301012\"}},\"variant\":\"text\",\"column\":0,\"row\":7,\"width\":32,\"height\":3,\"in_board_index\":4,\"background\":\"default\"},{\"flow\":{\"tag\":\"image_13\",\"data\":{\"mode\":\"fit\",\"url\":\"https://www.telemetryapp.com/img/telemetry-logo-header.png\"}},\"variant\":\"image\",\"column\":0,\"row\":0,\"width\":6,\"height\":7,\"in_board_index\":5,\"background\":\"default\"},{\"flow\":{\"tag\":\"gauge_22\",\"data\":{\"max\":100,\"value\":50}},\"variant\":\"gauge\",\"column\":26,\"row\":0,\"width\":6,\"height\":7,\"in_board_index\":6,\"background\":\"default\"},{\"flow\":{\"tag\":\"value_50\",\"data\":{\"label\":\"Approved\",\"value\":23163}},\"variant\":\"value\",\"column\":0,\"row\":9,\"width\":11,\"height\":4,\"in_board_index\":7,\"background\":\"default\"},{\"flow\":{\"tag\":\"value_51\",\"data\":{\"label\":\"Declined\",\"value\":6287}},\"variant\":\"value\",\"column\":11,\"row\":9,\"width\":10,\"height\":4,\"in_board_index\":8,\"background\":\"default\"},{\"flow\":{\"tag\":\"value_52\",\"data\":{\"label\":\"Error\",\"value\":1050}},\"variant\":\"value\",\"column\":21,\"row\":9,\"width\":11,\"height\":4,\"in_board_index\":9,\"background\":\"default\"},{\"flow\":{\"tag\":\"timeseries_9\",\"data\":{\"interval\":\"hours\",\"interval_count\":0,\"renderer\":\"line\",\"series_metadata\":[{\"aggregation\":\"avg\"},{\"aggregation\":\"avg\"},{\"aggregation\":\"avg\"}],\"values\":[100,200,300]}},\"variant\":\"timeseries\",\"column\":6,\"row\":0,\"width\":10,\"height\":7,\"in_board_index\":10,\"background\":\"default\"},{\"flow\":{\"tag\":\"timeseries_10\",\"data\":{\"interval\":\"hours\",\"interval_count\":0,\"renderer\":\"line\",\"series_metadata\":[{\"aggregation\":\"avg\"},{\"aggregation\":\"avg\"},{\"aggregation\":\"avg\"}],\"values\":[100,200,300]}},\"variant\":\"timeseries\",\"column\":16,\"row\":0,\"width\":10,\"height\":7,\"in_board_index\":11,\"background\":\"default\"}]}"
+	
 	board, err := job.GetOrCreateBoard(boardName, boardPrefix, template)
 
 	if err != nil {
@@ -51,11 +51,11 @@ func (plugin *MockFdPlugin) Init(job *job.Job) error {
 		return err
 	}
 
-	if err = plugin.PluginHelper.AddTaskWithClosureFromBoardForFlowWithTag(plugin.FeedTimeSeriesFlow, time.Second*5, board, "timeseries_9"); err != nil {
+	if err = plugin.PluginHelper.AddTaskWithClosureFromBoardForFlowWithTag(plugin.FeedTimeSeriesFlowAPITransactionsPerMinute, time.Second*5, board, "timeseries_9"); err != nil {
 		return err
 	}
 
-	if err = plugin.PluginHelper.AddTaskWithClosureFromBoardForFlowWithTag(plugin.FeedTimeSeriesFlow, time.Second*5, board, "timeseries_10"); err != nil {
+	if err = plugin.PluginHelper.AddTaskWithClosureFromBoardForFlowWithTag(plugin.FeedTimeSeriesFlowHCOTransactionsPerMinute, time.Second*5, board, "timeseries_10"); err != nil {
 		return err
 	}
 
@@ -103,7 +103,48 @@ func (plugin *MockFdPlugin) FeedImageFlow(job *job.Job, flow *gotelemetry.Flow) 
 	job.Logf("Updated flow %s", flow.Tag)
 }
 
-func (plugin *MockFdPlugin) FeedTimeSeriesFlow(job *job.Job, flow *gotelemetry.Flow) {
+func (plugin *MockFdPlugin) FeedTimeSeriesFlowAPITransactionsPerMinute(job *job.Job, flow *gotelemetry.Flow) {
+
+	data, err := flow.TimeseriesData()
+	if !err {
+		job.ReportError(gotelemetry.NewError(500, "Cannot extract value data from flow with tag `"+flow.Tag+"`"))
+		return
+	}
+
+	data.Title = "API transactions per minute"
+
+	data.SeriesMetadata[0].Label = "Approved"
+	data.SeriesMetadata[0].Color = "8de2x26"
+
+	data.SeriesMetadata[1].Label = "Declined"
+	data.SeriesMetadata[1].Color = "2aa4e5"
+
+	data.SeriesMetadata[2].Label = "Error"
+	data.SeriesMetadata[2].Color = "ff543b"
+
+	job.PostFlowUpdate(flow)
+	job.Logf("Updated flow %s", flow.Tag)
+}
+
+func (plugin *MockFdPlugin) FeedTimeSeriesFlowHCOTransactionsPerMinute(job *job.Job, flow *gotelemetry.Flow) {
+
+	data, err := flow.TimeseriesData()
+	if !err {
+		job.ReportError(gotelemetry.NewError(500, "Cannot extract value data from flow with tag `"+flow.Tag+"`"))
+		return
+	}
+
+	data.Title = "HCO transactions per minute"
+
+	data.SeriesMetadata[0].Label = "Approved"
+	data.SeriesMetadata[0].Color = "8de226"
+
+	data.SeriesMetadata[1].Label = "Declined"
+	data.SeriesMetadata[1].Color = "2aa4e5"
+
+	data.SeriesMetadata[2].Label = "Error"
+	data.SeriesMetadata[2].Color = "ff543b"
+
 	job.PostFlowUpdate(flow)
 	job.Logf("Updated flow %s", flow.Tag)
 }
